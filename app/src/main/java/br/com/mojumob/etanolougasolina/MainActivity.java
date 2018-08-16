c49b75c6f6dd82934b0ee95bcfda7e7a542e26c7
bytecore@gmail.com <mo10128>
2018-08-16 15:58:12 -0300

    Criando atributos e fazendo as inicializações

--- app/src/main/java/br/com/mojumob/etanolougasolina/MainActivity.java
+++ app/src/main/java/br/com/mojumob/etanolougasolina/MainActivity.java
@@ -2,12 +2,33 @@ package br.com.mojumob.etanolougasolina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Atributos
+    private EditText edtEtanol, edtGasolina, edtMediaEtanol, edtMediaGasolina;
+    private Button btnCalcular, btnLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializaçoes
+        edtGasolina      = findViewById(R.id.edtGasolina);
+        edtEtanol        = findViewById(R.id.edtEtanol);
+        edtMediaGasolina = findViewById(R.id.edtGasolina);
+        edtMediaEtanol   = findViewById(R.id.edtMediaEtanol);
+        btnCalcular      = findViewById(R.id.btnCalcular);
+        btnLimpar        = findViewById(R.id.btnLimpar);
+
+
+        //Evento clique no botão Calcular
+
+        //Evento clique no botão Limpar
+
     }
 }
