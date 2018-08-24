463b6446e8c665760c041962c7b8c796ad27437e
bytecore@gmail.com <mo10128>
2018-08-23 23:12:19 -0300

    Cria os atributos da classe Combustivel

--- app/src/main/java/br/com/mojumob/etanolougasolina/model/Combustivel.java
+++ app/src/main/java/br/com/mojumob/etanolougasolina/model/Combustivel.java
@@ -1,4 +1,31 @@
 package br.com.mojumob.etanolougasolina.model;
 
 public class Combustivel {
+
+    String tipoCombistivel;
+    String preco;
+
+    public Combustivel() {
+    }
+
+    public Combustivel(String tipoCombistivel, String preco) {
+        this.tipoCombistivel = tipoCombistivel;
+        this.preco = preco;
+    }
+
+    public String getTipoCombistivel() {
+        return tipoCombistivel;
+    }
+
+    public void setTipoCombistivel(String tipoCombistivel) {
+        this.tipoCombistivel = tipoCombistivel;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
 }
