1bcfe7177707051309590dd5daa21e1014cd0639
bytecore@gmail.com <mo10128>
2018-08-26 19:52:40 -0300

    Cria metodos para view e presenter

--- app/src/main/java/br/com/mojumob/etanolougasolina/task/Calcula.java
+++ app/src/main/java/br/com/mojumob/etanolougasolina/task/Calcula.java
@@ -1,4 +1,24 @@
 package br.com.mojumob.etanolougasolina.task;
 
 public interface Calcula {
+
+    interface View{
+        void erroValorEtanolEmBranco();
+        void erroValorGasolinaEmBranco();
+        void calculoNormal();
+        void erroValorMediaEtanolVazio();
+        void erroValorMediaGasolinaVazio();
+        void calculoPersonalizado();
+        void escolhaEtanol();
+        void escolhaGasolina();
+
+    }
+
+    interface Presenter{
+        void validaCampos();
+        void calculaNormal();
+        void formataNumeroDeDoubleParaString();
+        void calculaPersonalizado();
+    }
+
 }
