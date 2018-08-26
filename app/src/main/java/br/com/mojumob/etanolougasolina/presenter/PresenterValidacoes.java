bbcea41297594427667ff31231a25e689b4f1ffd
bytecore@gmail.com <mo10128>
2018-08-26 11:48:06 -0300

    Ajusta metodo de calculo do combustivel

--- app/src/main/java/br/com/mojumob/etanolougasolina/presenter/PresenterValidacoes.java
+++ app/src/main/java/br/com/mojumob/etanolougasolina/presenter/PresenterValidacoes.java
@@ -18,21 +18,29 @@ public class PresenterValidacoes {
             mainActivity.erroValorGasolinaEmBranco();
         }else if (mediaEtanol.isEmpty() && mediaGasolina.isEmpty()){
             //Calculo realizado com a media padrao, pois o usuario não colocou sua media de comsumo
-            calculaMelhorCombustivel(false);
+            calculaMelhorCombustivel(false, etanol, gasolina, mediaEtanol, mediaGasolina);
         }else if(mediaEtanol.isEmpty()){
             mainActivity.erroValorMediaEtanolVazio();
         }else if(mediaGasolina.isEmpty()){
             mainActivity.erroValorMediaGasolinaVazio();
         }else{
-            calculaMelhorCombustivel(true);
+            calculaMelhorCombustivel(true, etanol, gasolina, mediaEtanol, mediaGasolina);
         }
     }
 
-    private void calculaMelhorCombustivel(boolean consumoPersonalizado) {
+    private void calculaMelhorCombustivel(boolean consumoPersonalizado,
+                                          String etanol,
+                                          String gasolina,
+                                          String mediaEtanol,
+                                          String merdiaGasolina) {
 
         if(consumoPersonalizado){
             mainActivity.showMelhorCombustivelPeronalizado();
         }else{
+
+            //Consumo padrão 70%
+
+
             mainActivity.showMelhorCombustivel();
         }
     }
